44a9d5c052882a8c16b38f4caacbe053cc36a208
unknown <jamzmarks@gmail.com>
2025-01-12 10:53:37 -0300

    feat user and reports entity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { UsersModule } from 'src/users/users.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { Reports } from 'src/reports/reports.entity';
 
 @Module({
     imports: [
@@ -12,7 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         TypeOrmModule.forRoot({
             type: 'sqlite',
             database: 'db.sqlite',
-            entities: [],
+            entities: [User, Reports],
             synchronize: true
         })
     ],
