7be63cceb5f7b5de724ec648c85a88488ca26abd
JamzMarks <jamzmarks@gmail.com>
2025-01-17 17:27:00 -0300

    test<Auth> add Signup and Whoami test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -15,7 +15,7 @@ describe('Auth Controller (2e2)', () => {
         await app.init();
     });
 
-    it('Handles a signup reques', () => {
+    it('Handles a signup request', () => {
         let email = 'atasdad@asdas.com'
         return request(app.getHttpServer())
             .post('/auth/signup')
@@ -26,5 +26,25 @@ describe('Auth Controller (2e2)', () => {
                 expect(id).toBeDefined();
                 expect(email).toEqual(email)
             })
-    })  
-})
+    }) 
+    it('Signup as a new user then get currently logged in user', async () => {
+        const email = 'newemail@mail.com';
+
+        const res = await request(app.getHttpServer())
+            .post('/auth/signup')
+            .send({
+                email,
+                password: 'asdf'
+            })
+            .expect(201)
+
+        const cookie = res.get('Set-Cookie');
+
+        const { body } = await request(app.getHttpServer())
+            .get('/auth/whoami')
+            .set('Cookie', cookie)
+            .expect(200)
+
+        expect(body.email).toEqual(email)
+    }); 
+});
