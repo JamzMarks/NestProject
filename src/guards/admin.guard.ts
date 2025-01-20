c88ff7597edf4b961ad86c0eced8acff641a07b5
JamzMarks <jamzmarks@gmail.com>
2025-01-20 10:47:52 -0300

    fix<AdminGuard> fix admin guard doesn't work properly

--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -1,13 +1,12 @@
 import { CanActivate, ExecutionContext } from "@nestjs/common";
 
-export class AdminGuard {
-    CanActivate(context: ExecutionContext){
+export class AdminGuard implements CanActivate{
+    canActivate(context: ExecutionContext){
         const request = context.switchToHttp().getRequest();
 
         if(!request.currentUser){
             return false;
         }
-
         return request.currentUser.admin;
     }
 }
